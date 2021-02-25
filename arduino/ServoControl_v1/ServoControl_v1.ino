d0d9b6352fdd35ab9429767ddaf8482ea89c57c7
wisehackermonkey <oranbusiness@gmail.com>
2021-02-25 00:05:28 +0000

    [add] ros build files

--- arduino/ServoControl_v1/ServoControl_v1.ino
+++ arduino/ServoControl_v1/ServoControl_v1.ino
@@ -1,65 +1,96 @@
+// /*
+//  * Controlling ros arduino node to control 2 servo motors
+//  * by oran collins
+//  * github.com/wisehackermonkey
+//  * oranbusiness@gmail.com
+//  * 20210224
+//  * This sketch demonstrates the control of hobby R/C servos
+//  * using ROS and the arduiono
+//  * 
+//  * For the full tutorial write up, visit
+//  * www.ros.org/wiki/rosserial_arduino_demos
+//  *
+//  * For more information on the Arduino Servo Library
+//  * Checkout :
+//  * http://www.arduino.cc/en/Reference/Servo
+//  */
+
+// #if (ARDUINO >= 100)
+//  #include <Arduino.h>
+// #else
+//  #include <WProgram.h>
+// #endif
+
+// #include <Servo.h> 
+// #include <ros.h>
+// #include <std_msgs/Float32.h>
+// #include <std_msgs/String.h>
+
+// String HEAD_SEND_TOPIC = "debug_head_position";
+// String HEAD_RECEIVE_TOPIC = "head_servo_position";
+
+// ros::NodeHandle  nh;
+
+// std_msgs:String debug_message;
+
+// Servo servo;
+
+// void servo_cb( const std_msgs::Float32& cmd_msg){
+//   servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
+//   digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
+// }
+
+
+
+// ros::Subscriber<std_msgs::Float32> sub(HEAD_RECEIVE_TOPIC, servo_cb);
+// // allow sending data back to computer for debugging purposes on ros topic "head_debug"
+// ros::Publisher debug_position(HEAD_SEND_TOPIC, &debug_message);
+
+// void setup(){
+//   pinMode(13, OUTPUT);
+
+//   nh.initNode();
+//   nh.subscribe(sub);
+//   nh.advertise(debug_message);
+
+//   debug_message.data = "Works!";
+//   servo.attach(9); //attach it to pin 9
+// }
+
+// void loop(){
+  
+//   debug_position.publish(&debug_message);
+//   nh.spinOnce();
+//   delay(1);
+// }
+
 /*
- * Controlling ros arduino node to control 2 servo motors
- * by oran collins
- * github.com/wisehackermonkey
- * oranbusiness@gmail.com
- * 20210224
- * This sketch demonstrates the control of hobby R/C servos
- * using ROS and the arduiono
- * 
- * For the full tutorial write up, visit
- * www.ros.org/wiki/rosserial_arduino_demos
- *
- * For more information on the Arduino Servo Library
- * Checkout :
- * http://www.arduino.cc/en/Reference/Servo
+ * rosserial Publisher Example
+ * Prints "hello world!"
  */
 
-#if (ARDUINO >= 100)
- #include <Arduino.h>
-#else
- #include <WProgram.h>
-#endif
-
-#include <Servo.h> 
 #include <ros.h>
-#include <std_msgs/Float32.msg>
-#include <std_msgs/String.msg>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
 
-String HEAD_SEND_TOPIC = "debug_head_position";
-String HEAD_RECEIVE_TOPIC = "head_servo_position";
 
 ros::NodeHandle  nh;
 
-std_msgs:String debug_message;
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
 
-Servo servo;
+char hello[13] = "hello world!";
 
-void servo_cb( const std_msgs::Float32& cmd_msg){
-  servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
-  digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
-}
-
-
-
-ros::Subscriber<std_msgs::Float32> sub(HEAD_RECEIVE_TOPIC, servo_cb);
-// allow sending data back to computer for debugging purposes on ros topic "head_debug"
-ros::Publisher debug_position(HEAD_SEND_TOPIC, &debug_message);
-
-void setup(){
-  pinMode(13, OUTPUT);
-
+void setup()
+{
   nh.initNode();
-  nh.subscribe(sub);
-  nh.advertize(debug_message);
-
-  debug_message.data = "Works!";
-  servo.attach(9); //attach it to pin 9
+  nh.advertise(chatter);
 }
 
-void loop(){
-  
-  debug_position.publish(&debug_message);
+void loop()
+{
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
   nh.spinOnce();
-  delay(1);
+  delay(1000);
 }
--- scripts/servo_control.py
+++ scripts/servo_control.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+
+#!/usr/bin/env python3
 
 # creates a ros node that controls two servo motors
 #    on a arduino running ros serial 
@@ -11,18 +12,19 @@
 
 import rospy
 
-from std_msgs.msg from String, Float32
+from std_msgs.msg import String, Float32
 
 motor1_position = 0.0
 # just prints the current positions of the motors
 def motor_positions_callback(msg):
     print(msg.data)
 
-if __main__ =="__main__":
+if __name__ == "__main__":
     rospy.init_node("servo_mover")
 
     pub=rospy.Publisher("head_servo_position", Float32, queue_size=1)
-
+    rate=rospy.Rate(10)
+    print("servo_mover: Running")
     while not rospy.is_shutdown():
         if motor1_position > 180:
             motor1_position = 0
