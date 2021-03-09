da749f84b5b26ea7c0d65c69d273a87ec66ef14f
wisehackermonkey <oranbusiness@gmail.com>
2021-03-08 16:19:11 -0800

    [add] servo move topic stuff

--- scripts/servo_control.py
+++ scripts/servo_control.py
@@ -1,38 +1,50 @@
 #!/usr/bin/env python3
 
 # creates a ros node that controls two servo motors
-#    on a arduino running ros serial 
+# on a arduino running ros serial 
+# this node accepts JointState which is
+# --------------- 
+# would the datastructure look like
+# jointStateObj.name = ["neck_yaw", "neck_pitch"]
+# jointStateObj.position = [180,0]
+# jointStateObj.velocity# unused
+# jointStateObj.effort#unused
+# ---------------
 # by oran collins
 # github.com/wisehackermonkey
 # oranbusiness@gmail.com
-# 20210224
+# 20210307
 
 
 
 import rospy
+import math
 
-from std_msgs.msg import String, UInt16
+from std_msgs.msg import UInt16
+from sensor_msgs.msg import JointState
 
-motor1_position = 0
-# just prints the current positions of the motors
-def motor_positions_callback(msg):
-    print(msg.data)
+
+global yaw_servo
+global sub
+global yaw_servo_position
+yaw_servo_position = UInt16()
+
+def move_servos(msg):
+    servo_yaw =int(msg.position[0])
+    print(msg)
+    print(servo_yaw)
+    inteager_yaw_angle = 0
+    yaw_servo_position.data = int(msg.position[0])
+    # send an int angle to move the servo position to 0-180  
+    yaw_servo.publish(yaw_servo_position)
 
 if __name__ == "__main__":
-    rospy.init_node("servo_mover")
+    rospy.init_node("head_mover")
+    yaw_servo = rospy.Publisher("servo1",UInt16, queue_size=1)
 
-    pub=rospy.Publisher("servo1", UInt16, queue_size=1)
-    pub2=rospy.Publisher("servo2", UInt16, queue_size=1)
-    # sub=rospy.Subscriber("")
+    sub=rospy.Subscriber("move_head",JointState,move_servos)
     rate=rospy.Rate(10)
     print("servo_mover: Running")
     while not rospy.is_shutdown():
-        if motor1_position > 180:
-            motor1_position = 0
-        pub.publish(motor1_position)
-        pub2.publish(motor1_position)
-        motor1_position += 1
-        
         rate.sleep()
-# [std_msgs/UInt16] vs. 
-# [std_msgs/Int16]{'callerid': '/serial_node', 'md5sum': '1df79edf208b629fe6b81923a544552d', 'message_definition': 'uint16 data\n', 'tcp_nodelay': '0', 'topic': '/servo', 'type': 'std_msgs/UInt16'}
+    
